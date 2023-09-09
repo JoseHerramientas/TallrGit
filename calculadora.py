d701947893f03273d5be54d83d1b7f2ef02a7312
Jose David Mojica Ballesteros <000530048@bga.upb>
2023-09-09 13:56:20 -0500

    se agrego la resta

--- calculadora.py
+++ calculadora.py
@@ -25,4 +25,11 @@ if seleccion == 1:
         print(f"El resultado de la suma es: {resultado}")
     except ValueError:
         print("Por favor, ingrese números válidos.")
-
+elif seleccion == 2: 
+    try:
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+        resultado = num1 - num2
+        print(f"El resultado de la resta es: {resultado}")
+    except ValueError:
+        print("Por favor, ingrese números válidos.")
