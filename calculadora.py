8e1e08382e45c5ab78144baea87da87e4ab47c07
Jose David Mojica Ballesteros <000530048@bga.upb>
2023-09-09 10:31:47 -0500

    Se creo mensaje inicial y suma

---
+++ calculadora.py
@@ -0,0 +1,28 @@
+print("""
+
+Seleccione qué operación quiere hacer (solo el número):
+      1 suma.
+      2 resta.
+      3 multiplicación.
+      4 división.
+
+""")
+
+seleccion = 0
+while seleccion not in [1, 2, 3, 4]:
+    try:
+        seleccion = int(input("Ingrese el número de la operación deseada: "))
+        if seleccion not in [1, 2, 3, 4]:
+            print("Por favor, ingrese un número válido (1, 2, 3 o 4).")
+    except ValueError:
+        print("Por favor, ingrese un número válido (1, 2, 3 o 4).")
+
+if seleccion == 1: 
+    try:
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+        resultado = num1 + num2
+        print(f"El resultado de la suma es: {resultado}")
+    except ValueError:
+        print("Por favor, ingrese números válidos.")
+
